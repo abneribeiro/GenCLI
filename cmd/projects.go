daf354320cfa0251ba8f7c7ee58bed73433e1b9e
seu nome <eliasabner38@gmail.com>
2024-03-02 19:41:30 +0000

    feat: Add projects command to generate projects from the terminal

---
+++ cmd/projects.go
@@ -0,0 +1,20 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// ProjectsCmd represents the projects command
+var ProjectsCmd = &cobra.Command{
+	Use:   "projects",
+	Short: "generate your projects from the terminal",
+	Long: `terminalProjects is a CLI application that allows you to generate your projects from the terminal.`,
+	Run: genProjects,
+}
+
+func init() {
+	rootCmd.AddCommand(ProjectsCmd)
+
+}
+
+func genProjects(cmd *cobra.Command, args []string) {
+	
+}
