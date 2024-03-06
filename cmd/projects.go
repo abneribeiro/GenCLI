581a0cdf588c9a64aa04223c85bd844c2e8cb5de
seu nome <eliasabner38@gmail.com>
2024-03-06 21:35:21 +0000

    feat: Refactor project generation code

--- cmd/projects.go
+++ cmd/projects.go
@@ -3,16 +3,19 @@ package cmd
 import (
 	"log"
 	"os"
-
+	"fmt"
+	"path/filepath"
+	"strings"
 	"github.com/spf13/cobra"
 )
 
 var projectsCmd = &cobra.Command{
 	Use:   "proj",
 	Short: "generate your projects from the terminal",
-	Long: `terminalProjects is a CLI application that allows you to generate your projects from the terminal.
+	Long: `GenCli is a CLI application that allows you to generate your projects from the terminal.
+
 	For example:
-	terminalProjects projects -n myProject -t .go
+	terminalProjects projects -n myProject.go
 	`,
 	Run: genProjects,
 }
@@ -22,29 +25,38 @@ func init() {
 	rootCmd.AddCommand(projectsCmd)
 
 	projectsCmd.Flags().StringP("name", "n","","Specify the programming language")
-	projectsCmd.Flags().StringP("type", "t","","Specify the programming language")
 
 	projectsCmd.MarkFlagRequired("name")
-	projectsCmd.MarkFlagRequired("type")
 
 }
+
 
 func genProjects(cmd *cobra.Command, args []string) {
 	projectName, _ := cmd.Flags().GetString("name")
-	projectType, _ := cmd.Flags().GetString("type")
 
-	project := projectName + projectType
+	// Validate input
+	if projectName == "" {
+		log.Fatal("Project name is required")
+	}
 
-	file, err := os.Create(project)
+	// Extract file extension
+	fileExt := filepath.Ext(projectName)
+	if fileExt == "" {
+		log.Fatal("Invalid file extension")
+	}
 
+	// Remove leading dot from file extension
+	fileExt = strings.TrimPrefix(fileExt, ".")
+
+	file, err := os.Create(projectName)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer file.Close()
 
 	log.Println("File created successfully")
-
+	fmt.Println("File Extension:", fileExt)
 }
+
 
 
